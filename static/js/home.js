c5fe7f5249d8cd98c8d79b449dd2edf2c85fb6d4
Glenn Flanagan <hello@glenn.codes>
2017-11-30 14:51:14 +0000

    Remove console.logs

--- static/js/home.js
+++ static/js/home.js
@@ -21,10 +21,8 @@ $(function () {
         e.preventDefault();
         var goTo = $(this).data('slide');
         if (goTo == "prev") {
-            console.log('back');
             $('#carousel').carousel('prev');
         } else {
-            console.log('forward');
             $('#carousel').carousel('next');
         }
     });
@@ -36,7 +34,6 @@ var activeSlide = 1;
 var totalSlides = $('#carousel .item').length;
 
 $('#carousel').on('slide.bs.carousel', function (carousel) {
-    console.log(carousel.direction);
     if (carousel.direction == "right") {
         activeSlide--;
         if (activeSlide < 1) {
