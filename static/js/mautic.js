6c3b8c90a3151ae41980be82f1970bec90a8a73e
Glenn Flanagan <hello@glenn.codes>
2018-10-04 15:05:02 +0100

    Correct Subscription form styles

--- static/js/mautic.js
+++ static/js/mautic.js
@@ -1,29 +1,32 @@
-if (typeof MauticSDKLoaded == 'undefined') {
-    var MauticSDKLoaded = true;
-    var head            = document.getElementsByTagName('head')[0];
-    var script          = document.createElement('script');
-    script.type         = 'text/javascript';
-    script.src          = 'https://jetstack.mautic.net/mautic/media/js/mautic-form.js';
-    script.onload       = function() {
-        MauticSDK.onLoad();
-    };
-    head.appendChild(script);
-    var MauticDomain = 'https://jetstack.mautic.net';
-    var MauticLang   = {
-        'submittingMessage': "Please wait..."
-    }
+if (typeof MauticSDKLoaded == "undefined") {
+  var MauticSDKLoaded = true;
+  var head = document.getElementsByTagName("head")[0];
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  script.src = "https://jetstack.mautic.net/mautic/media/js/mautic-form.js";
+  script.onload = function() {
+    MauticSDK.onLoad();
+  };
+  head.appendChild(script);
+  var MauticDomain = "https://jetstack.mautic.net";
+  var MauticLang = {
+    submittingMessage: "Please wait..."
+  };
 }
 
 function initHandleMauticFormReset() {
-    var form = document.querySelector('.mauticform_wrapper form');
-    var message = document.querySelector('.mauticform-message');
-    form.addEventListener('reset', function() {
-        message.scrollIntoView({ behavior: 'smooth' });
-    });
+  var form = document.querySelector(".mauticform_wrapper form");
+  var message = document.querySelector(".mauticform-message");
+  form.addEventListener("reset", function() {
+    message.scrollIntoView({ behavior: "smooth" });
+    // Remove subscription-form__input--has-value class to reset styles on subscription page
+    $(form)
+      .find(".subscription-form__input--has-value")
+      .removeClass("subscription-form__input--has-value");
+  });
 }
 
-$(function () {
-    // Add event listener to the form so we can handle when Mautic clears the values on success.
-    initHandleMauticFormReset();
+$(function() {
+  // Add event listener to the form so we can handle when Mautic clears the values on success.
+  initHandleMauticFormReset();
 });
-  
