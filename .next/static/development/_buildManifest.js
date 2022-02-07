5eaacf5601aacea339b9baac71609e0fe50653af
Patrick Heneise <patrick@zentered.co>
2022-02-07 10:35:48 +0200

    add legacy docs
    Signed-off-by: Patrick Heneise <patrick@zentered.co>
    
    style(legacy-docs): add css and js
    
    Update all links for docs in the public folder to point to `/legacy-docs.css`
    and `/main.js`.

---
+++ .next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/docs/[[...slug]]":["static\u002Fchunks\u002Fpages\u002Fdocs\u002F[[...slug]].js"],sortedPages:["\u002F_app","\u002F_error","\u002Fdocs\u002F[[...slug]]"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
