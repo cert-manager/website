129b95b59ea063bd09c1d5aab1db621d3f0f55dc
Ashley Davis <ashley.davis@jetstack.io>
2022-04-13 16:01:42 +0100

    Update scripts in scripts/bin
    - Removes hugo and multiversion, since they're no longer used
    - Updates the version of htmltest
    - Updates comments
    
    Signed-off-by: Ashley Davis <ashley.davis@jetstack.io>

--- scripts/bin/lib.sh
+++ scripts/bin/lib.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2019 The Jetstack cert-manager contributors.
+# Copyright 2022 The cert-manager Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,9 +13,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# This script constructs a 'content/' directory that contains content for all
-# configured versions of the documentation.
 
 set -o errexit
 set -o nounset
