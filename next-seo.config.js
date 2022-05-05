cd9d1f99a0a6f5294f15fa59cd985ab6055af524
Maël Valais <mael@vls.dev>
2022-05-05 14:06:44 +0200

    use NEXT_PUBLIC_URL to configure the site base URL
    This fixes the sitemap.xml that is currently totally broken, as well as
    the og:image tag on deploy builds that didn't have the correct URL.
    
    Signed-off-by: Maël Valais <mael@vls.dev>

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,14 +1,12 @@
-const deployUrl = 'https://cert-manager.io'
-
 export default {
   openGraph: {
     type: 'website',
     locale: 'en_EN',
-    url: `${deployUrl}`,
+    url: `${process.env.NEXT_PUBLIC_URL}`,
     site_name: 'cert-manager',
     images: [
       {
-        url: `${deployUrl}/images/og1.png`,
+        url: `${process.env.NEXT_PUBLIC_URL}/images/og1.png`,
         width: 1200,
         height: 630,
         alt: 'cert-manager — automated Kubernetes X.509 certificates'
