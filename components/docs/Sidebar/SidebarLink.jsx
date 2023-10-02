4ae300ba92be56b416df73f809f26e3294832e5a
Tim Ramlot <42113979+inteon@users.noreply.github.com>
2023-10-02 16:30:23 +0200

    fix missing logic to unfold side menu when going to the link directly
    Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

--- components/docs/Sidebar/SidebarLink.jsx
+++ components/docs/Sidebar/SidebarLink.jsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import classNames from 'classnames'
@@ -6,7 +7,7 @@ export default function SidebarLink({
   href,
   caption,
   parentOpen = true,
-  setSidebarCollapsed
+  setParentOpen
 }) {
   const router = useRouter()
   const active = router.asPath === href + '/'
@@ -14,12 +15,16 @@ export default function SidebarLink({
     'flex text-dark-2 hover:text-blue-2 text-base py-2 transition ease-in-out duration-150 no-underline': true,
     'font-medium opacity-60 w-full': active
   })
+
+  useEffect(() => {
+    if (active) setParentOpen(true)
+  }, []);
+
   return (
     (<Link
       href={href}
       className={linkClasses}
-      tabIndex={parentOpen ? 0 : -1}
-      onClick={() => setSidebarCollapsed(true)}>
+      tabIndex={parentOpen ? 0 : -1}>
 
       {caption}
 
