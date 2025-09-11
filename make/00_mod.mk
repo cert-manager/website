4b544f3a31974678049b4a6c8892995c3beb6c3c
Tim Ramlot <42113979+inteon@users.noreply.github.com>
2025-09-11 08:15:25 +0200

    upgrade nodejs
    Signed-off-by: Tim Ramlot <42113979+inteon@users.noreply.github.com>

--- make/00_mod.mk
+++ make/00_mod.mk
@@ -15,5 +15,5 @@
 repo_name := github.com/cert-manager/website
 
 # Node and NPM are installed from the same source, so the versions must match
-ADDITIONAL_TOOLS += node=v20.11.1
-ADDITIONAL_TOOLS += npm=v20.11.1
+ADDITIONAL_TOOLS += node=v22.19.0
+ADDITIONAL_TOOLS += npm=v22.19.0
